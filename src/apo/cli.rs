bd743d4b15150c701d23fbe22a12bada5499cb8e
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-08-28 06:36:25 +0900

    fix: incDecrimental select

--- src/apo/cli.rs
+++ src/apo/cli.rs
@@ -29,65 +29,54 @@ pub fn build_cli() -> App<'static, 'static> {
         )
         .arg(//day
 			Arg::with_name("day")
-                .help("set Day")
+                .help("set Day - prefix +/- is relative sector")
                 .short("d")
                 .long("day")
                 .takes_value(true)
-        )
-        .arg(
-			Arg::with_name("Future Date")
-                .help("How many days from Today")
-                .short(">")
-                .takes_value(true)
-        )
-        .arg(
-			Arg::with_name("Previous Date")
-                .help("How many days ago")
-                .short("<")
-                .takes_value(true)
-        )
-        .subcommand(//add
-			SubCommand::with_name("add")
-                .about("Add Apointment")
-                .arg(Arg::with_name("descriptions")
-                    .help("hh:MM [Type][Important][Recururse] TEXT")
-                    .last(true)
-                )
-        )
-        .subcommand(//move
-			SubCommand::with_name("mv")
-            .about("Move to another time")
-            .arg(Arg::with_name("from_time")
-                    .help("hh:MM - move Targeted Time")
-                    .conflicts_with("to_time")
-                    .required(true)
+    )
+    ////////////////////////////////////
+    .subcommand(//add
+        SubCommand::with_name("add")
+            .about("Add Apointment")
+            .arg(Arg::with_name("descriptions")
+                .help("hh:MM [Type][Important][Recururse] TEXT")
+                .last(true)
             )
-            .arg(Arg::with_name("to_time")
-                    .help("hh:MM - Moved Time")
-                    .required(true)
+    )
+    .subcommand(//move
+        SubCommand::with_name("mv")
+        .about("Move to another time")
+        .arg(Arg::with_name("from_time")
+                .help("hh:MM - move Targeted Time")
+                .conflicts_with("to_time")
+                .required(true)
+        )
+        .arg(Arg::with_name("to_time")
+                .help("hh:MM - Moved Time")
+                .required(true)
+        )
+    )
+    .subcommand(//remove
+        SubCommand::with_name("rm")
+            .about("Remove Apointment")
+            .arg(Arg::with_name("time")
+                .help("hh:MM")
+                .required(true)
             )
-        )
-        .subcommand(//remove
-	        SubCommand::with_name("rm")
-                .about("Remove Apointment")
-                .arg(Arg::with_name("time")
-                    .help("hh:MM")
-                    .required(true)
-                )
-        )
-        .subcommand(//edit
-		    SubCommand::with_name("edit")
-                .about("edit Apointments by a day")
-        )
-        .subcommand(//modify
-		   	SubCommand::with_name("modify")
-                .about("modify apointment")
-                .arg(Arg::with_name("time")
-                    .help("hh:MM - edit time")
-                    .required(true)
-                )
-                .args_from_usage(
-                    "type      -t <TYPE>    'type 1:Schedule 2:Reminder 3:DeadLine'
+    )
+    .subcommand(//edit
+        SubCommand::with_name("edit")
+            .about("edit Apointments by a day")
+    )
+    .subcommand(//modify
+        SubCommand::with_name("modify")
+            .about("modify apointment")
+            .arg(Arg::with_name("time")
+                .help("hh:MM - edit time")
+                .required(true)
+            )
+            .args_from_usage(
+                "type      -t <TYPE>    'type 1:Schedule 2:Reminder 3:DeadLine'
                      important -i <Boolean> 'important: true or false'
                      recurse   -r <Boolean> 'recurse: true or false'"
                 )
@@ -98,9 +87,9 @@ pub fn build_cli() -> App<'static, 'static> {
                 .arg(Arg::with_name("time")
                     .help("hh:MM")
                 )
-            )
-        /*
-        .group(ArgGroup::with_name("days")
-            .args(&["today", "tom", "Future Date","Previous Date"])
-        )*/
+        )
+        .subcommand(//note
+		    SubCommand::with_name("sync")
+                .about("syncronize to remote repository by git")
+        )
 }
--- src/apo/main.rs
+++ src/apo/main.rs
@@ -2,8 +2,34 @@
 extern crate clap; // args analyze library.
 mod cli; // imp cli module.
 
-fn main() {
-    let matches = cli::build_cli().get_matches();
-    println!("HELLO")
+use std::process;
 
+fn main() {
+    let _matches = cli::build_cli().get_matches(); // Clap Args Analyzer.
+    //println!("Invalid Argments!");
+    //process::exit(1);
+    // DateObj
+    let _toyear  = 2020;
+    let _tomonth = 8;
+    let _today   = 25;
+
+    let _year  = if let Some(o) = _matches.value_of("year")  { dateDetector(o,_toyear) } else { _toyear  };
+    let _month = if let Some(o) = _matches.value_of("month") { dateDetector(o,_tomonth)} else { _tomonth };
+    let _day   = if let Some(o) = _matches.value_of("day")   { dateDetector(o,_today)  } else { _today   };
+
+    println!("TEST DATE DEF: {0} {1} {2}",_year,_month,_day);
+
+    // ReDef Date
+}
+
+fn dateDetector(arg: &str, nowVal: i64) -> i64 {
+    if arg.chars().nth(0).unwrap() == '+' {
+        let argVal: i64 = arg.chars().skip(1).collect::<String>().parse().unwrap();
+        return nowVal + argVal;
+    }
+    if arg.chars().nth(0).unwrap() == '-' {
+        let argVal: i64 = arg.chars().skip(1).collect::<String>().parse().unwrap();
+        return nowVal - argVal;
+    }
+    return arg.parse().unwrap();
 }
