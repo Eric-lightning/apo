2ae472c33b6c84ce04e4b082948dd6c0104c30d1
Eric-lightning <19676712+Eric-lightning@users.noreply.github.com>
2020-08-31 06:16:12 +0900

    imp ymd Opts

--- src/apo/main.rs
+++ src/apo/main.rs
@@ -2,27 +2,49 @@
 extern crate clap; // args analyze library.
 mod cli; // imp cli module.
 
-use std::process;
+use chrono::{TimeZone, Weekday, ParseResult,Duration};
+use chrono::prelude::{DateTime, Utc, Local, Datelike, Timelike};
+use chrono::offset::FixedOffset;
 
 fn main() {
     let _matches = cli::build_cli().get_matches(); // Clap Args Analyzer.
     //println!("Invalid Argments!");
     //process::exit(1);
-    // DateObj
-    let _toyear  = 2020;
-    let _tomonth = 8;
-    let _today   = 25;
+    let mut date: DateTime<Local> = Local::now();
 
-    let _year  = if let Some(o) = _matches.value_of("year")  { dateDetector(o,_toyear) } else { _toyear  };
-    let _month = if let Some(o) = _matches.value_of("month") { dateDetector(o,_tomonth)} else { _tomonth };
-    let _day   = if let Some(o) = _matches.value_of("day")   { dateDetector(o,_today)  } else { _today   };
+    if let Some(o) = _matches.value_of("day") {
+        if prefix_one_char(o) == '+' { date = date + Duration::days(skip_one_str_to_i64(o)); }
+        else if prefix_one_char(o) == '-' { date = date + Duration::days(0 - skip_one_str_to_i64(o)); }
+        else { date = Local.ymd(date.year(), date.month(), o.parse().unwrap()).and_hms(0,00,00); }
+    };
+    if let Some(o) = _matches.value_of("month") {
+        if prefix_one_char(o) == '+' { date = date + Duration::weeks(4 * skip_one_str_to_i64(o)); }
+        else if prefix_one_char(o) == '-' { date = date + Duration::weeks(4 * (0 - skip_one_str_to_i64(o))); }
+        else { date = Local.ymd(date.year(),o.parse().unwrap(), date.day()).and_hms(0,00,00); }
+    };
+    if let Some(o) = _matches.value_of("year")  {
+        if prefix_one_char(o) == '+' { date = date + Duration::weeks(12 * 4 * skip_one_str_to_i64(o)); }
+        else if prefix_one_char(o) == '-' { date = date - Duration::weeks(12 * 4 * skip_one_str_to_i64(o)); }
+    };
+    if let Some(o) = _matches.value_of("week") {
+        if prefix_one_char(o) == '+' { date = date + Duration::weeks(skip_one_str_to_i64(o)); }
+        else if prefix_one_char(o) == '-' { date = date + Duration::weeks(0 - skip_one_str_to_i64(o)); }
+        else { date = Local.ymd(o.parse().unwrap(), date.month(), date.day()).and_hms(0,00,00); }
+    };
 
-    println!("TEST DATE DEF: {0} {1} {2}",_year,_month,_day);
+    /////////////////////
+    println!("TEST DATE DEF: {0} {1} {2}",date.year(),date.month(),date.day()); //DEBUG
 
-    // ReDef Date
+
 }
-
-fn dateDetector(arg: &str, nowVal: i64) -> i64 {
+fn prefix_one_char(arg: &str) -> char {
+    return arg.chars().nth(0).unwrap();
+}
+fn skip_one_str_to_i64(arg: &str) -> i64 {
+    return arg.chars().skip(1).collect::<String>().parse().unwrap();
+}
+/*
+fn date_detector(arg: &str, nowVal: i64) -> i64 {
     if arg.chars().nth(0).unwrap() == '+' {
         let argVal: i64 = arg.chars().skip(1).collect::<String>().parse().unwrap();
         return nowVal + argVal;
@@ -33,3 +55,4 @@ fn dateDetector(arg: &str, nowVal: i64) -> i64 {
     }
     return arg.parse().unwrap();
 }
+*/
